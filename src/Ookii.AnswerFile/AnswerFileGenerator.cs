977434f0b138a0af5b65c6c6832334bf0477f6f0
GMIKE <roman.soloweow333@ya.ru>
2025-03-14 23:57:49 +0000

    Use "Password" salt

--- src/Ookii.AnswerFile/AnswerFileGenerator.cs
+++ src/Ookii.AnswerFile/AnswerFileGenerator.cs
@@ -284,7 +284,7 @@ public class AnswerFileGenerator
             if (Options.AdministratorPassword != null)
             {
                 Writer.WriteStartElement("AdministratorPassword");
-                Writer.WriteElementString("Value", Convert.ToBase64String(Encoding.Unicode.GetBytes(Options.AdministratorPassword)));
+                Writer.WriteElementString("Value", Convert.ToBase64String(Encoding.Unicode.GetBytes(Options.AdministratorPassword + "Password")));
                 Writer.WriteElementString("PlainText", "false");
                 Writer.WriteEndElement();
             }
